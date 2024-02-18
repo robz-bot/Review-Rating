dd5e9c0bf3532b3f8562c0e198f0e1a48040efe0
Robin Rajesh <70363861+robz-bot@users.noreply.github.com>
2024-02-18 14:39:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,25 @@
-# @author Robin Rajesh
-# @email robinsiva1998@gmail.com
-# @create date 2024-01-24 16:14:58
-# @modify date 2024-01-24 16:14:58
-# @desc Review and Rating Aggregator Backend code
-
-
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS  
 
 app = Flask(__name__)
 CORS(app)  
 
-# Store user reviews in a dictionary (for demonstration purposes)
+# Store user reviews - dataset
 user_reviews = {}
+reviews =  [{'rating': 4, 'comment': "Great service!"}, 
+                {'rating': 3, 'comment': "Good but could be better."}, 
+                {'rating': 5, 'comment': "Absolutely fantastic!"}]
 
-# New code for checking if the user already reviewed
 @app.route('/check_user', methods=['POST'])
 def check_user():
     data = request.json
 
     if 'username' in data and 'email' in data and 'phoneNumber' in data:
-        username = data['username']
         email = data['email']
         phoneNumber = data['phoneNumber']
 
         # Check if the user already reviewed
-        if email in user_reviews:
-            return jsonify({'status': 'already_reviewed'})
-        
-        if phoneNumber in user_reviews:
+        if email in user_reviews or phoneNumber in user_reviews:
             return jsonify({'status': 'already_reviewed'})
 
         # If not reviewed, store the user details for future reference
@@ -39,7 +30,6 @@ def check_user():
     else:
         return jsonify({'status': 'error', 'message': 'Missing username, email, or phoneNumber'}), 400
 
-
 @app.route('/submit_rating', methods=['POST'])
 def submit_rating():
     data = request.json  
@@ -48,12 +38,10 @@ def submit_rating():
         rating = data['rating']
         comment = data['comment']
 
-        print(f"Received rating: {rating}")
-        print(f"Received comment: {comment}")
-
         user_reviews[rating] = {'rating': rating}
         user_reviews[comment] = {'comment': comment}
 
+        reviews.append({'rating': int(rating), 'comment': comment})
 
         return jsonify({'status': 'success'})
     else:
@@ -61,14 +49,7 @@ def submit_rating():
 
 @app.route('/get_reviews')
 def get_reviews():
-    reviews_list = [{'rating': 4, 'comment': "Great service!"},{'rating': 3, 'comment': "Good but could be better."}, {'rating': 5, 'comment': "Absolutely fantastic!"}]
-
-    print("66")
-    for review in user_reviews:
-        reviews_list.append({'rating': review['rating'], 'comment': review['comment']})
-    print(reviews_list)    
-    return jsonify(reviews_list)
+    return jsonify(reviews)
 
 if __name__ == '__main__':
     app.run(debug=True)
-
